a47fcb8978edc8a7f7471251e3623aff1bedea9c
daily1313 <kimsb7218@naver.com>
2025-08-11 23:21:13 +0900

    feat: implement Q2 Problem

--- src/main/java/com/example/stream/q2/Q2.java
+++ src/main/java/com/example/stream/q2/Q2.java
@@ -1,9 +1,9 @@
 package com.example.stream.q2;
 
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Q2 {
 
@@ -12,12 +12,26 @@ public class Q2 {
     // 2.1 List에 저장된 단어들의 접두사가 각각 몇개씩 있는지 Map<String, Integer>으로 변환하여라.
     // ex) ("T", 1), ("a", 2) ...
     public Map<String, Integer> quiz1() {
-        return new HashMap<>();
+        Map<String, Integer> results = WORDS.stream()
+                .map(word -> word.substring(0, 1))
+                .collect(Collectors.toMap(key -> key, value -> 1, Integer::sum));
+
+        results.entrySet().forEach(entry -> {
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+        });
+
+        return results;
     }
 
     // 2.2 List에 저장된 단어들 중에서 단어의 길이가 2 이상인 경우에만, 모든 단어를 대문자로 변환하여 스페이스로 구분한 하나의 문자열로 합한 결과를 반환하여라.
     // ex) ["Hello", "a", "Island", "b"] -> “H I”
     public String quiz2() {
-        return null;
+        String result = WORDS.stream()
+                .filter(word -> word.length() >= 2)
+                .map(word -> word.substring(0, 1).toUpperCase())
+                .collect(Collectors.joining(" "));
+
+        System.out.println(result);
+        return result;
     }
 }
