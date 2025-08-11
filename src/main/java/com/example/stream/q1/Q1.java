c20e5961cf5510b7383cd80e0840cd14118bbddb
daily1313 <kimsb7218@naver.com>
2025-08-11 23:12:44 +0900

    feat: implement Q1 Problem

--- src/main/java/com/example/stream/q1/Q1.java
+++ src/main/java/com/example/stream/q1/Q1.java
@@ -4,28 +4,63 @@ import com.opencsv.CSVReader;
 
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Q1 {
 
     // 1.1 각 취미를 선호하는 인원이 몇 명인지 계산하여라.
     public Map<String, Integer> quiz1() throws IOException {
         List<String[]> csvLines = readCsvLines();
-        return new HashMap<>();
+
+        Map<String, Integer> results = csvLines.stream()
+                .flatMap(line -> Arrays.stream(line[1].replaceAll("\\s", "").split(":")))
+                .collect(Collectors.toMap(k -> k, v -> 1, Integer::sum));
+
+        results.entrySet().forEach(entry -> {
+            System.out.println(entry.getKey() + ":" + entry.getValue());
+        });
+
+        return results;
     }
 
     // 1.2 각 취미를 선호하는 정씨 성을 갖는 인원이 몇 명인지 계산하여라.
     public Map<String, Integer> quiz2() throws IOException {
         List<String[]> csvLines = readCsvLines();
-        return new HashMap<>();
+
+        Map<String, Integer> results = csvLines.stream()
+                .filter(name -> name[0].startsWith("정"))
+                .flatMap(line -> Arrays.stream(line[1].replaceAll("\\s", "").split(":")))
+                .collect(Collectors.toMap(k -> k, v -> 1, Integer::sum));
+
+        results.entrySet().forEach(entry -> {
+            System.out.println(entry.getKey() + ":" + entry.getValue());
+        });
+
+        return results;
     }
 
     // 1.3 소개 내용에 '좋아'가 몇번 등장하는지 계산하여라.
     public int quiz3() throws IOException {
         List<String[]> csvLines = readCsvLines();
-        return 0;
+
+        int count = csvLines.stream()
+                .mapToInt(line -> countKeyword(line[2], "좋아"))
+                .sum();
+
+        return count;
+    }
+
+    private int countKeyword(String source, String target) {
+        int index = source.indexOf(target);
+        if(index == -1) {
+            return 0;
+        } else {
+            return 1 + countKeyword(source.substring(index + 1), target);
+        }
     }
 
     private List<String[]> readCsvLines() throws IOException {
