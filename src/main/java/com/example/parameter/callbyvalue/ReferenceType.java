579221c88bb2eaf32eb3980eda31affa92c5e75a
daily1313 <kimsb7218@naver.com>
2025-08-31 16:46:14 +0900

    feat: add call by value simple example code (reference type)

---
+++ src/main/java/com/example/parameter/callbyvalue/ReferenceType.java
@@ -0,0 +1,38 @@
+package com.example.parameter.callbyvalue;
+
+public class ReferenceType {
+
+    public static void main(String[] args) {
+        A a = new A(1);
+        B b = new B(1);
+
+        System.out.println(a.num == 1);
+        System.out.println(b.num == 1);
+
+        modify(a, b);
+        System.out.println(a.num == 2);
+        System.out.println(b.num == 1);
+    }
+
+    private static class A {
+        public int num;
+
+        public A(int num) {
+            this.num = num;
+        }
+    }
+
+    private static class B {
+        public int num;
+        public B(int num) {
+            this.num = num;
+        }
+    }
+
+    private static void modify(A a, B b) {
+        a.num++;
+
+        b = new B(1);
+        b.num++;
+    }
+}
