ba67be165238dd076520822e476923d515737018
daily1313 <kimsb7218@naver.com>
2025-09-26 01:27:55 +0900

    feat: add client execution code

--- src/main/java/com/example/socket/ClientApplication.java
+++ src/main/java/com/example/socket/ClientApplication.java
@@ -1,4 +1,10 @@
 package com.example.socket;
 
+import com.example.socket.client.Client;
+
 public class ClientApplication {
+
+    public static void main(String[] args) {
+        new Client().run();
+    }
 }
